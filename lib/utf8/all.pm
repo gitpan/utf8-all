43de2baed0e141f6f26bf3201c58a4ae8a509176
Mike Doherty <doherty@cpan.org>
2012-07-29 20:18:11 +0000

    Import of DOHERTY/utf8-all-0.006 from CPAN.
    gitpan-cpan-distribution: utf8-all
    gitpan-cpan-version:      0.006
    gitpan-cpan-path:         DOHERTY/utf8-all-0.006.tar.gz
    gitpan-cpan-author:       DOHERTY
    gitpan-cpan-maturity:     released

--- lib/utf8/all.pm
+++ lib/utf8/all.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use 5.010; # state
 # ABSTRACT: turn on Unicode - all of it
-our $VERSION = '0.005'; # VERSION
+our $VERSION = '0.006'; # VERSION
 
 
 use Import::Into;
@@ -48,7 +48,7 @@ utf8::all - turn on Unicode - all of it
 
 =head1 VERSION
 
-version 0.005
+version 0.006
 
 =head1 SYNOPSIS
 
